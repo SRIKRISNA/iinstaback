7535b0593c9e861df2c4dd4635064ce56e89c7b2
Srikrishna <krishna333sp@gmail.com>
2023-04-26 10:18:41 +0530

    all

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const app = express();
 
 //middleware 
 app.use(express.json({limit: "30mb", extended: true}));
-app.use(cors({
-    origin:["http://localhost:3000", "https://insta-client333.onrender.com/"],
-}));
+app.use(cors());
 app.use(express.urlencoded({extended:false}));
 
 //database
